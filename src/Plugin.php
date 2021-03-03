7bb602ee5feafaf049abf6af1a22fe592e954ff6
Marvin Poggensee <marvin@max-tours.de>
2021-03-03 19:15:29 +0100

    Added Plugin.php

---
+++ src/Plugin.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Pggns\CleanerBake;
+
+use Cake\Console\CommandCollection;
+use Cake\Core\BasePlugin;
+use Cake\Core\PluginApplicationInterface;
+
+class Plugin extends BasePlugin {
+	/**
+	 * @param \Cake\Core\PluginApplicationInterface $app PluginApplicationInterface
+	 * @return void
+	 */
+	public function bootstrap(PluginApplicationInterface $app): void {
+		parent::bootstrap($app);
+	}
+	
+	/**
+	 * @param \Cake\Console\CommandCollection $commands CommandCollection
+	 * @return \Cake\Console\CommandCollection
+	 */
+	public function console(CommandCollection $commands): CommandCollection {
+		return $commands;
+	}
+}
