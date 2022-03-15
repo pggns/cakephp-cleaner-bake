c86924558dcb7e3b28830c6005d34294c3e4f790
Marvin Poggensee <git@pggns.de>
2022-03-15 08:56:05 +0100

    Removed config file

--- config/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Cake\Core\Configure;
-
-Configure::write('Pggns.CleanerBake.stringifyList.indent',			2);
-Configure::write('Pggns.CleanerBake.stringifyList.tab',				'	');
-Configure::write('Pggns.CleanerBake.stringifyList.tabLength',		4);
-Configure::write('Pggns.CleanerBake.stringifyList.trailingComma',	true);
-Configure::write('Pggns.CleanerBake.stringifyList.quotes',			true);
-Configure::write('Pggns.CleanerBake.stringifyList.align',			true);
